a030b9d07c5e940346d5534c12453558fd427bdc
Ourai Lin <ourairyu@hotmail.com>
2015-02-03 16:43:14 +0800

    修复拼写错误

--- test/test.js
+++ test/test.js
@@ -3,8 +3,8 @@ $(document).ready(function() {
   H5F.errors({
     COULD_NOT_BE_EMPTY: "不能为空",
     UNKNOWN_INPUT_TYPE: "未知类型",
-    LENGTH_SMALLER_THEN_MINIMUM: "长度超出最小长度",
-    LENGTH_BIGGER_THEN_MAXIMUM: "长度超出最大长度",
+    LENGTH_SMALLER_THAN_MINIMUM: "长度超出最小长度",
+    LENGTH_BIGGER_THAN_MAXIMUM: "长度超出最大长度",
     INVALID_VALUE: "无效值",
     NOT_A_NUMBER: "不是数字",
     UNDERFLOW: "下溢",
